1c98d2c3ef214c4be9caddbc25fc84e33e45ead2
Dixon Adair <dixonadairv@gmail.com>
2015-09-14 13:34:46 -0700

    commit

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,3 +5,10 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+
+# added by me acc'ding to Rails Tutorial:
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
+
+# Explanation from Rails Tutorial:
+# Upon encountering an error or failing test, the test runner shows a “stack trace” or “backtrace” that traces the course of a failed test through the application. While this backtrace is usually very useful for tracking down the problem, on some systems (including the cloud IDE) it goes past the application code and into the various gem dependencies, including Rails itself. The resulting backtrace is often inconveniently long, especially since the source of the problem is usually the application and not one of its dependencies.
+# The solution is to filter the backtrace to eliminate unwanted lines. This requires the mini_backtrace gem included in Listing 3.2, combined with a backtrace silencer. On the cloud IDE, most unwanted lines contain the string rvm (referring to the Ruby Version Manager), so I recommend using the silencer shown in Listing 3.41 to filter them out.
